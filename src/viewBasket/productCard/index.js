1796f4691139dfb2d64ee1f20e964bda65e24845
AlfieDarko <me@alfiedarko.co.uk>
2020-10-01 20:53:45 +0100

    added styling with styled system via chakraui

--- src/viewBasket/productCard/index.js
+++ src/viewBasket/productCard/index.js
@@ -5,6 +5,50 @@ const ProductCard = ({ imageSRC, title, description, price, quantity, id }) => {
   const [state, dispatch] = useProductBasket()
   return (
     <Flex data-testid={`product-card-${id}`} >
+      <Flex mb={8} pb={10} borderBottom="4px" borderStyle="dashed" borderColor="lightgrey">
+        <Box>
+          <Image
+            size={["100px", "100px", "150px", "150px"]}
+            display={['none', 'initial', 'initial', 'initial']}
+            p={[2, 4, 6, 6]}
+            objectFit="cover"
+            src={imageSRC}
+            alt={`image of ${title}`}
+            data-testid={`image-${id}`}
+          />
+        </Box>
+
+        <Box display="flex" justifyContent='space-between' flexDirection="column" >
+          <Stack spacing={2} justifyContent="space-between" flexDirection="column" >
+            <Stack flexDirection="row" justifyContent="inherit">
+              <Heading as="h3" fontSize={["sm", "md", "lg", "xl"]} data-testid={`title-${id}`}>
+                {title}
+              </Heading>
+              <Text pl={24} fontWeight="bold" as="span" color="#E28A5A" data-testid={`price-${id}`}>{priceFormatter(price)}</Text>
+            </Stack>
+            <Text fontSize="sm" width="350px" data-testid={`description-${id}`}>
+              {description}
+            </Text>
+          </Stack>
+
+          <Flex data-testid="quantity" justifyContent="space-between">
+            <Flex alignItems="center" pr={12} >Quantity: </Flex>
+            <Box display="flex">
+              <Button p="0" onClick={(e) => onClickHandler(e, id, +1)} data-testid={`increase-quantity-btn-${id}`}>
+                <Icon name="triangle-up" size="20px" />
+              </Button>
+              <Flex alignItems="center" px="3" data-testid={`product-quantity-${id}`}>{quantity}</Flex>
+
+              <Button p="0" disabled={quantity === 0} data-testid={`decrease-quantity-btn-${id}`} onClick={(e) => onClickHandler(e, id, -1)} >
+                <Icon name="triangle-down" size="20px" />
+              </Button>
+            </Box>
+          </Flex>
+        </Box>
+
+
+      </Flex>
+
     </Flex >
   )
 }
