cebc91d86b43d3d3ab97fdc3beea6f8308e9d118
AlfieDarko <me@alfiedarko.co.uk>
2020-10-01 20:53:02 +0100

    added product card base component

--- src/viewBasket/productCard/index.js
+++ src/viewBasket/productCard/index.js
@@ -1,3 +1,22 @@
 import React from 'react'
 import { Box, Flex, Text, Heading, Image, Stack, Icon, Button } from '@chakra-ui/core'
 import { useProductBasket } from '../../context/context'
+const ProductCard = ({ imageSRC, title, description, price, quantity, id }) => {
+  const [state, dispatch] = useProductBasket()
+  return (
+    <Flex data-testid={`product-card-${id}`} >
+    </Flex >
+  )
+}
+
+ProductCard.propTypes = {
+  imageSRC: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired
+}
+
+
+export default ProductCard
